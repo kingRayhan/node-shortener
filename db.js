3a369a3b1dec51989caced1e41d17a355db41bd8
kingrayhan <rayhan095@gmail.com>
2020-11-21 03:13:46 +0600

    database url

--- db.js
+++ db.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 require("colors");
 
 mongoose
-  .connect(
-    "mongodb+srv://nodejs-shortener:nodejs-shortener@cluster0.tl0w4.mongodb.net/nodejs-shortener",
-    {
-      useNewUrlParser: true,
-    }
-  )
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
   .then((c) => {
     console.log("Database connected".green);
   })
